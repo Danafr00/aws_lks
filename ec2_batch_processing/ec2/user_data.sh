425d510ec9ad4859876ec20acd2dcedd500167bf
Danafr00 <67647526+Danafr00@users.noreply.github.com>
2026-04-09 13:43:59 +0700

    fixing

--- ec2_batch_processing/ec2/user_data.sh
+++ ec2_batch_processing/ec2/user_data.sh
@@ -3,7 +3,7 @@
 apt update -y
 apt install python3-pip -y
 
-pip3 install boto3 psycopg2-binary --break-system-packages
+pip3 install boto3 psycopg2-binary
 
 cat <<EOF > /home/ubuntu/process.py
 <PASTE process.py CONTENT HERE>
